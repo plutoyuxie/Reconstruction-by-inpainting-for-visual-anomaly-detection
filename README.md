8cbc102f24eb237dfa230da061afc4b9285fdad2
user <346929320@qq.com>
2020-11-09 18:57:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Reconstruction-by-inpainting-for-visual-anomaly-detection
+
+This is an unofficial implementation of the paper **Reconstruction by inpainting for visual anomaly detection**.<br>
+
+# PipeLine
+![avatar](./pipeline.png) 
+
+# Visualization demo
+Please check this **[mosaic.ipynb](./mosaic.ipynb)** file
+
+# To be continued
---
+++ utils/gen_mask.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def gen_mask(k_list, n, im_size):
+    while True:
+        Ms = []
+        for k in k_list:
+            N = im_size // k
+            rdn = np.random.permutation(N**2)
+            additive = N**2 % n
+            if additive > 0:
+                rdn = np.concatenate((rdn, np.asarray([-1]*(n-additive))))
+            n_index = rdn.reshape(n,-1)
+            for index in n_index:
+                tmp = [0 if i in index else 1 for i in range(N**2)]
+                tmp = np.asarray(tmp).reshape(N,N)
+                tmp = tmp.repeat(k,0).repeat(k,1)
+                Ms.append(tmp)
+        yield Ms
